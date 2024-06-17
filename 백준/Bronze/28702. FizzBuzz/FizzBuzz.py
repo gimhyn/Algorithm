6da2925acc5803ada57417e0db5aba094c8f7ba0
HYN <158463565+gimhyn@users.noreply.github.com>
2024-06-17 15:56:52 +0900

    [Bronze I] Title: FizzBuzz, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -1,28 +1,13 @@
-nums = [input() for _ in range(3)]
+num1 = input()
+num2 = input()
+num3 = input()
 
-
-new_nums = []
-for i in range(3):
-    if 'zz' not in nums[i]:
-        res = int(nums[i])+(3 - i)
-        break
-    elif nums[i] == 'Fizz':
-        new_nums.append(3)
-    elif nums[i] == 'Buzz':
-        new_nums.append(5)
-    else:
-        new_nums.append(15)
-
-while True:
-    if res:
-        break
-    for j in range(10000000-1):
-        a = new_nums[0] * j
-        b = a + 1
-        c = b + 1
-        if b % new_nums[1] == 0 and c % new_nums[2] == 0:
-            res = c + 1
-            break
+if num1.isdigit():
+    res = int(num1)+3
+elif num2.isdigit():
+    res = int(num2)+2
+else:
+    res = int(num3)+1
 
 if res % 15 == 0:
     print('FizzBuzz')
