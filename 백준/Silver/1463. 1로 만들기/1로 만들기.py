b761c69897a7576f5aec63125b10ddc511c9ac4e
HYN <158463565+gimhyn@users.noreply.github.com>
2024-06-27 14:38:43 +0900

    [Silver III] Title: 1로 만들기, Time: 364 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,19 +1,24 @@
 def findmin(num):
-    for i in range(2, num+1):
-        count[i] = count[i - 1] + 1
+    dp = [0]*(num+1)
+    if num == 1:
+        return 0
+    elif num < 4:
+        return 1
+    else:
+        dp[2] = 1
+        dp[3] = 1
+        for i in range(4, num+1):
+            dp[i] = dp[i-1] + 1
+            if i % 3 == 0:
+                dp[i] = min(dp[i], dp[i//3] + 1)
+            if i % 2 == 0:
+                dp[i] = min(dp[i], dp[i//2] + 1)
 
-        if i % 3 == 0:
-            count[i] = min(count[i], count[i//3] + 1)
-
-        if i % 2 == 0:
-            count[i] = min(count[i], count[i//2] + 1)
-
-    return count[num]
+    return dp[num]
 
 
 import sys
 input = sys.stdin.readline
-count = [0]*(10**6 + 1)
 
 N = int(input())
 print(findmin(N))
