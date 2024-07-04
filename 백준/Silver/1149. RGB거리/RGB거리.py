689fa4a7748981c4824d3cc80b88b54d2d144163
HYN <158463565+gimhyn@users.noreply.github.com>
2024-07-04 21:26:47 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,14 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
-# 2 <= N <= 1000
-costs = [list(map(int, input().split())) for _ in range(N)]
-dp = [[0, 0, 0] for _ in range(N)]
+N = int(input())
+costs = []
+
+for i in range(N):
+    r, g, b = map(int, input().strip().split())
+    costs. append([r, g, b])
+
+dp = [[0] * 3 for _ in range(N)]
+
 dp[0] = costs[0]
-for i in range(1, N):
-    dp[i][0] = costs[i][0] + min(dp[i-1][1], dp[i-1][2])
-    dp[i][1] = costs[i][1] + min(dp[i-1][0], dp[i-1][2])
-    dp[i][2] = costs[i][2] + min(dp[i-1][0], dp[i-1][1])
+for j in range(1, N):
+    for c in range(3):
+        if dp[j-1][(c+1)%3] < dp[j-1][(c+2)%3]:
+            dp[j][c] = dp[j-1][(c+1)%3] + costs[j][c]
+        else:
+            dp[j][c] = dp[j-1][(c+2)%3] + costs[j][c]
 
 print(min(dp[N-1]))
+
