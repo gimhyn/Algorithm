5c4ac8725b50ab50f2d4a32a2bf05716e4267c74
HYN <158463565+gimhyn@users.noreply.github.com>
2024-07-20 20:47:59 +0900

    [Silver II] Title: 좌표 압축, Time: 940 ms, Memory: 273524 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -3,24 +3,7 @@ input = sys.stdin.readline
 
 N = int(input().strip())
 X = list(map(int, input().strip().split()))
-my_dict = {}
-Y = sorted(X)
-cnt = 0
-num = 0
-for i in range(N):
-    if i < N-1 and Y[i] == Y[i+1]:
-        cnt += 1
-    else:
-        for j in range(i-cnt, i+1):
-            my_dict[Y[j]] = num
-        cnt = 0
-        num += 1
-
+Y = sorted(list(set(X))) # 중복 제거용
+my_dict = {Y[i]: i for i in range(len(Y))}
 for x in X:
-    print(my_dict[x], end= ' ')
-'''
-1 <= N <= 1,000,0000
-시작은 무조건 0
-그다음으로 작은 거 1
-sort했을 때 index순
-'''
+    print(my_dict[x], end=' ')
