49cd2a4fcb1d5be22c07c27dfb895fce6d578c14
HYN <158463565+gimhyn@users.noreply.github.com>
2024-08-28 18:01:45 +0900

    [Silver I] Title: 숨바꼭질, Time: 80 ms, Memory: 34268 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, K = map(int, input().strip().split())
+
+def bfs():
+    if N >= K:
+        return N - K
+    
+    q = deque()
+    q.append((N, 0))  # (location, time)
+    visited = [False] * 100001
+    visited[N] = True
+    
+    while q:
+        location, time = q.popleft()
+        
+        for nl in (location * 2, location - 1, location + 1):
+            if nl == K:
+                return time + 1
+            if 0 <= nl < 100001 and not visited[nl]:
+                visited[nl] = True
+                q.append((nl, time + 1))
+
+print(bfs())
