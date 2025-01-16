52be7cd970c56a164623af3e440209ac71890c3b
HYN <158463565+gimhyn@users.noreply.github.com>
2025-01-16 12:45:51 +0900

    [Gold III] Title: 파티, Time: 52 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -3,9 +3,8 @@ import heapq
 
 input = sys.stdin.readline
 
-def dijkstra(start):
-    """시작 노드에서 모든 노드까지의 최단 거리를 계산하는 함수"""
-    distance = [float('inf')] * (n + 1)
+def dijkstra(start, graph):
+    distance = [float('inf')] * (n+1)
     q = []
     heapq.heappush(q, (0, start))
     distance[start] = 0
@@ -15,31 +14,29 @@ def dijkstra(start):
         if distance[now] < dist:
             continue
 
-        for cost, neighbor in graph[now]:
-            new_cost = dist + cost
-            if distance[neighbor] > new_cost:
-                distance[neighbor] = new_cost
-                heapq.heappush(q, (new_cost, neighbor))
+        for node in graph[now]:
+            cost = dist + node[0]
+            if distance[node[1]] > cost:
+                distance[node[1]] = cost
+                heapq.heappush(q, (cost, node[1]))
 
     return distance
 
-# 입력 처리
 n, m, x = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+g = [[] for _ in range(n+1)]
+reverse_g = [[] for _ in range(n+1)] 
 
 for _ in range(m):
-    src, dest, time = map(int, input().split())
-    graph[src].append((time, dest))
+    start, end, time = map(int, input().split())
+    g[start].append((time, end))
+    reverse_g[end].append((time, start))
 
-# x에서 각 노드로 가는 최단 거리 계산
-from_x = dijkstra(x)
+togo = dijkstra(x, g)
+tocome = dijkstra(x, reverse_g)
 
-# 각 노드에서 x로 가는 최단 거리 계산
-to_x = [0] * (n + 1)
-for i in range(1, n + 1):
-    if i != x:  # 자기 자신에서 출발할 필요 없음
-        to_x[i] = dijkstra(i)[x]
+ans = 0
+for i in range(1, n+1):
+    if ans < togo[i] + tocome[i]:
+        ans = togo[i] + tocome[i]
 
-# 왕복 거리 계산 및 최대값 출력
-max_cost = max(from_x[i] + to_x[i] for i in range(1, n + 1))
-print(max_cost)
+print(ans)
