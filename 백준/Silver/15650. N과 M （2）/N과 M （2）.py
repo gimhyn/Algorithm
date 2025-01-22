fe7a0390460fba5975ed1730abb047a0050a635d
HYN <158463565+gimhyn@users.noreply.github.com>
2025-01-22 17:06:35 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,26 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def comb(lev):
-    if lev == M:
-        # print(path)
-        sequence = tuple(sorted(path))
-        res.add(sequence)
+N, M = map(int, input().split())
+
+used = [0] * (N+1)
+path = []
+def orders(n, m, start):
+    if len(path) == m:
+        print(*path)
         return
-    for i in range(1, N+1):
+    for i in range(start, n+1):
         if not used[i]:
             used[i] = 1
             path.append(i)
-            comb(lev+1)
+            orders(n, m, i)
             path.pop()
             used[i] = 0
 
 
-N, M = map(int, input().strip().split())
-path = []
-used = [0] * (N+1)
-res = set()
-comb(0)
-res = sorted(list(res))
-for seq in res:
-    print(*seq, end='\n')
+
+orders(N, M, 1)
