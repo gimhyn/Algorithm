d37de0053b706c41d5439a0407eb44e25dced6f2
HYN <158463565+gimhyn@users.noreply.github.com>
2025-02-04 13:21:35 +0900

    [Silver I] Title: 스티커, Time: 700 ms, Memory: 48752 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -4,28 +4,26 @@ input = sys.stdin.readline
 T = int(input())
 for tc in range(T):
     n = int(input())
-    # 1 <= n <= 100,000
-    stickers = list(list(map(int, input().strip().split())) for _ in range(2))
-		# 스티커 리스트를 만들어줍니다
-		
-    dp= [[0] * n for _ in range(2)]
-    # 똑같은 모양의 dp를 만들고, 
-    #윗줄 아랫줄 나눠서 왼쪽(작은 인덱스)부터 차례대로 스캔하며 해당 스티커까지 왔을 때 최댓값을 기록해 볼게요
-    
-    dp[0][0] = stickers[0][0]
-    dp[1][0] = stickers[1][0]
-		# n = 1이라면 dp는 당연히 해당 스티커 값
-		
-    if n > 1:
-        dp[0][1] = max(stickers[0][1] + stickers[1][0], stickers[0][0])
-        dp[1][1] = max(stickers[1][1] + stickers[0][0], stickers[1][0])
-        # 만약 n == 2이면 최댓값은 <대각선 방향 index 하나 전과 현재값을 더한 것>과
-        # <현재값 포기하고 하나 전 같은 라인만 선택한 것> 중 최댓값이 됩니다.
 
-        for i in range(2, n):
-            dp[0][i] = max(dp[1][i-1] + stickers[0][i], dp[0][i-1])
-            dp[1][i] = max(dp[0][i-1] + stickers[1][i], dp[1][i-1])
-						# n이 3이상이면 거기에 같은 라인에서 하나 전 인덱스를 포기하고 현재 값을 선택한 것까지 후보가 추가됩니다.
+    if n == 1:
+        a = int(input())
+        b = int(input())
+        print(max(a, b))
+        continue
 
-    print(max(dp[0][n-1], dp[1][n-1]))
-    # 두 줄 중 최댓값 고를게요
+    sticker1 = list(map(int, input().split()))
+    sticker2 = list(map(int, input().split()))
+
+    dp1 = [0] * n # 해당 칸을 색칠할 때 최댓값
+    dp2 = [0] * n
+
+    dp1[0], dp2[0] = sticker1[0], sticker2[0]
+
+    dp1[1] = dp2[0] + sticker1[1]
+    dp2[1] = dp1[0] + sticker2[1]
+
+    for i in range(2, n):
+        dp1[i] = max(dp2[i-1], dp1[i-2], dp2[i-2]) + sticker1[i]
+        dp2[i] = max(dp1[i-1], dp2[i-2], dp1[i-2]) + sticker2[i]
+
+    print(max(dp1[-1], dp2[-1]))
