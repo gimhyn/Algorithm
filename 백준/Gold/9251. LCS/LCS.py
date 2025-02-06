54ac71b4043cadca74118f36aeacf834e5fa3b07
HYN <158463565+gimhyn@users.noreply.github.com>
2025-02-06 12:37:18 +0900

    [Gold V] Title: LCS, Time: 436 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -4,15 +4,15 @@ input = sys.stdin.readline
 str1 = input().strip()
 str2 = input().strip()
 
-len1, len2 = len(str1), len(str2)
+len1 = len(str1)
+len2 = len(str2)
+dp = [[0]*(len2 + 1) for _ in range(len1 + 1)]
 
-dl = [[0] * (len2 + 1) for _ in range(len1 + 1)]
-
-for i in range(1, len1 + 1):
-    for j in range(1, len2 + 1):
-        if str1[i - 1] == str2[j - 1]:
-            dl[i][j] = dl[i - 1][j - 1] + 1
+for i in range(len1):
+    for j in range(len2):
+        if str2[j] == str1[i]:
+            dp[i+1][j+1] += dp[i][j] + 1
         else:
-            dl[i][j] = max(dl[i - 1][j], dl[i][j - 1])
+            dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
 
-print(dl[len1][len2])
+print(dp[len1][len2])
