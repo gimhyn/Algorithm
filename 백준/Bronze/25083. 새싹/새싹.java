939e9de1d89b5cf03559d25c27c06df06956e605
HYN <158463565+gimhyn@users.noreply.github.com>
2025-05-25 21:16:55 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = {
+            "         ,r'\"7",
+            "r`-_   ,'  ,/ ",
+            " \\. \". L_r' ",
+            "   `~\\/ ",
+            "      | ",
+            "      |"
+        };
+
+        for (String line : arr) {
+            System.out.println(line);
+        }
+    }
+}
