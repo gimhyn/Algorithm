251dd4712c4abf2171c917cde31a05c08c0ec8b4
HYN <158463565+gimhyn@users.noreply.github.com>
2025-06-03 22:39:23 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 428 ms, Memory: 48804 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N, K = map(int, input().split())
+
+def bfs(start, target):
+    q = deque([(0, start)])
+    visited = [0] * 100001
+    visited[start] = -1
+
+    res, min_turn = 0, 100001
+    while q:
+        turn, now = q.popleft()
+        dx = [1, -1, now]
+
+        if now == target:
+            if min_turn > turn:
+                min_turn = turn
+                res = 1
+                continue
+            elif min_turn == turn:
+                res += 1
+            else:
+                continue
+
+        for i in range(3):
+            next = now + dx[i]
+            if 0 <= next <= 100000 and (not visited[next] or visited[next] > turn):
+                q.append((turn+1, next))
+                visited[next] = turn + 1
+
+    return min_turn, res
+
+if N == K:
+    print(0)
+    print(1)
+else:
+    mt, cnt = bfs(N, K)
+    print(mt)
+    print(cnt)
